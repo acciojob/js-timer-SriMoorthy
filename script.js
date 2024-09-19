85fb1e843e46e747b9595c3013487cd9b7507413
SriMoorthy <srimoorthy27@gmail.com>
2024-09-19 13:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 function updateTime(){
 	let now = new Date();
 
-	const day = string(now.getDate()).padstart(2,'0');
-	const month = string(now.getMonth()+1).padstart(2,'0');
+	const day = String(now.getDate()).padstart(2,'0');
+	const month = String(now.getMonth()+1).padstart(2,'0');
 	const year = now.getFullYear();
 
-	const hours = string(now.getHours()).padstart(2,'0');
-	const min = string(now.getMinutes()).padstart(2,'0');
-	const seconds = string(now.getSecons()).padstart(2,'0')
+	const hours = String(now.getHours()).padstart(2,'0');
+	const min = String(now.getMinutes()).padstart(2,'0');
+	const seconds = String(now.getSecons()).padstart(2,'0')
 
 	const formatted = `${day}/${month}/${year},${hours}:${min}:${seconds}`
 	document.getElementById('timer').textContent = formatted;
