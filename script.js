5be2a356c491fc2fbf2bb0f7886bb79e643aef12
SriMoorthy <srimoorthy27@gmail.com>
2024-09-19 13:47:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // write js code here if required
+function updateTime(){
+	let now = new Date();
+
+	const day = string(now.getDate()).padstart(2,'0');
+	const month = string(now.getMonth()+1).padstart(2,'0');
+	const year = now.getFullYear();
+
+	const hours = string(now.getHours()).padstart(2,'0');
+	const min = string(now.getMinutes()).padstart(2,'0');
+	const seconds = string(now.getSecons()).padstart(2,'0')
+
+	const formatted = `${day}/${month}/${year},${hours}:${min}:${seconds}`
+	document.getElementById('timer').textContent = formatted;
+}
+
+setInterval(updateTime,1000);
+updateTime()
+
